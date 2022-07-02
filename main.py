457743aecebb3fb1e1e047df0f6ff1d3787d5da2
SilverAndSteel <littlejoker969.gmail.com>
2022-07-02 12:34:53 +0300

    update

--- main.py
+++ main.py
@@ -9,24 +9,34 @@ from kivy.core.window import Window
 Window.clearcolor = (224/255, 224/255, 224/255, 1)
 Window.title = "Randomizer"
 
+
 class Numbers(App):
 
     def __init__(self):
         super().__init__()
         self.label = Label(text="0", size_hint=(2, 3), pos_hint={'center_x': .5, 'center_y': .5},
                            color=(0/255, 0/255, 0/255, 1))
-
+        self.result = 0
     def button1_pressed(self, *args):
-        self.label.text = f"{str(randint(11, 99))} * {str(randint(11, 99))}"
+        a, b = randint(11, 99), randint(11, 99)
+        self.result = str(a * b)
+        self.label.text = f"{a} * {b}"
 
     def button2_pressed(self, *args):
-        self.label.text = f"{str(randint(101, 999))} * {str(randint(101, 999))}"
+        a, b = randint(101, 999), randint(101, 999)
+        self.result = str(a * b)
+        self.label.text = f"{a} * {b}"
 
     def button3_pressed(self, *args):
-        self.label.text = f"{str(randint(101, 999))} * {str(randint(11, 99))}"
+        a, b = randint(101, 999), randint(11, 99)
+        self.result = str(a * b)
+        self.label.text = f"{a} * {b}"
 
     def btn_reset(self, *args):
         self.label.text = "0"
+
+    def result_btn(self, *args):
+        self.label.text = self.result
 
     def build(self):
         box = BoxLayout(orientation='vertical')
@@ -43,14 +53,15 @@ class Numbers(App):
                         pos_hint={'center_x': .5, 'center_y': .3},
         )
         btn_4 = Button(text="Reset", on_press=self.btn_reset,
-                       pos_hint={'center_x': .5, 'center_y': .2},
+                       pos_hint={'center_x': .5, 'center_y': .1},
                        )
+        btn_5 = Button(text="Result", on_press=self.result_btn, pos_hint={'center_x': .5, 'center_y': .2})
         box.add_widget(self.label)
         box.add_widget(btn_1)
         box.add_widget(btn_2)
         box.add_widget(btn_3)
+        box.add_widget(btn_5)
         box.add_widget(btn_4)
-
 
         return box
 
